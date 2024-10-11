a2be42a4fff7256082bda67f2c4ead8ce389132a
mohamed <moonimooni132@gmail.com>
2024-10-11 22:21:01 +0300

    Fix multiplication bug

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
 # TODO 
-- Translate matrix thingy there's a problem with it
-- Projection matrices -> perspective, orthographic, look_at
+- Documentation 
+- Readme
--- examples/main.cpp
+++ examples/main.cpp
@@ -9,9 +9,4 @@
 #define MAT4_PRINT(m) for(int i = 0; i < 16; i++) {if(i % 4 == 0) {printf("\n");} printf("%f ", m[i]);} printf("\n")
 
 int main() {
-  soc::Matrix4 projection = soc::mat4_perspective(180.0f, 1920.0f / 1080.0f, 0.0f, 150.0f);
-  soc::Matrix4 view = soc::mat4_look_at(soc::Vector3(5.0f), soc::Vector3(0.0f, 0.0f, -3.0f), soc::Vector3(0.0f, 1.0f, 0.0f));
-  
-
-  MAT4_PRINT((projection * view));
 }
